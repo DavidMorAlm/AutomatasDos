b503f2695099fe35a94d6db71ceaf58e3a1d7abf
davidmoralm <david.moralm02@gmail.com>
2022-09-10 01:04:01 -0500

    First Commit

---
+++ Prueba.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+float resultado;
+float area, radio, pi;
+// Este programa calcula el volumen de un cilindro.
+void main()
+{
+    printf("radio = ");
+    scanf("%d", &radio);
+    pi = 3.141592653589793;
+    area = pi * (radio * radio);
+    printf("\nArea = ");
+    printf(area);
+}
